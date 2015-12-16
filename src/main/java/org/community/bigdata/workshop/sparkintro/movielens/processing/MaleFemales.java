77002e0eb7e584336d77c49536e0e6b6646b13d8
flo <flo.macicasan@gmail.com>
2015-12-16 19:45:55 +0200

    adding males & females

---
+++ src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/MaleFemales.java
@@ -0,0 +1,33 @@
+package org.community.bigdata.workshop.sparkintro.movielens.processing;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.community.bigdata.workshop.sparkintro.movielens.functions.conversion.UserConversion;
+import org.community.bigdata.workshop.sparkintro.movielens.functions.wordcount.WordPairFunction;
+import org.community.bigdata.workshop.sparkintro.movielens.functions.wordcount.WordSeparatorFunction;
+import org.community.bigdata.workshop.sparkintro.movielens.functions.wordcount.WordcountFunction;
+import scala.Tuple2;
+
+/**
+ * Created by tudorl on 07/12/15.
+ */
+public class MaleFemales {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[4]").setAppName(MaleFemales.class.getSimpleName());
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> records = sc.textFile("data/movielens/input/users");
+        JavaRDD<String> genders = records.map(new UserConversion()).map(i -> i.getGender());
+
+        System.out.println(genders.countByValue());
+
+
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
