8bcf25ed9f7847599b317bc4360e944e6b711eda
flo <flo.macicasan@gmail.com>
2015-12-16 19:52:56 +0200

    distribution of males and females

---
+++ src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/DistributionMalesFemales.java
@@ -0,0 +1,34 @@
+package org.community.bigdata.workshop.sparkintro.movielens.processing;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import org.community.bigdata.workshop.sparkintro.movielens.functions.conversion.UserConversion;
+import org.community.bigdata.workshop.sparkintro.movielens.model.User;
+import scala.Tuple2;
+
+/**
+ * Created by tudorl on 07/12/15.
+ */
+public class DistributionMalesFemales {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[4]").setAppName(DistributionMalesFemales.class.getSimpleName());
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> records = sc.textFile("data/movielens/input/users");
+        JavaPairRDD<Integer, String> rdds = records.map(new UserConversion()).mapToPair(
+                user -> new Tuple2<>(user.getAge(), user.getGender())
+        );
+
+        System.out.println(rdds.countByValue());
+
+
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
